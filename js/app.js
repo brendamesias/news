638a021ccce554e469bf8e68e7cf1d46444a6d83
brendamesias <brendamesiasvillar@gmail.com>
2018-02-14 11:08:38 -0500

    requerimientos reto 7 lms

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const searchFiled = document.getElementById('search-keyword');
 const responseContainer = document.getElementById('response-container');
 let searchForText;
 
+const articleRequest = new XMLHttpRequest();
+
 form.addEventListener('submit', function(e){
   e.preventDefault();
   responseContainer.innerHTML = '';
@@ -11,7 +13,6 @@ form.addEventListener('submit', function(e){
 });
 
 function getNews(){
-  const articleRequest = new XMLHttpRequest();
   articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchForText}&api-key=8f6d457603e4440587c864468d2e91dd`);
   articleRequest.onload = addNews;
   articleRequest.onerror = handleError;
@@ -23,14 +24,22 @@ function handleError(){
 }
 
 function addNews(){
-  const data = JSON.parse(this.responseText);
-  const article = data.response.docs[0];
-  const title = article.headline.main;
-  const snippet = article.snippet;
+  if (articleRequest.status === 200  && articleRequest.readyState === 4) {
+    const data = JSON.parse(this.responseText);
+    const article = data.response.docs;
+    for(let i = 0; i < article.length; i++) {
+      let title = article[i].headline.main;
+      let snippet = article[i].snippet;
+      let image = article[i].multimedia[2].url
 
-  let li = document.createElement('li');
-  li.className = 'articleClass';
-  li.innerText = snippet;
-
-  responseContainer.appendChild(li);
+  
+      let li = document.createElement('li');
+      let img = document.createElement('img');
+      li.className = 'articleClass';
+      li.innerText = snippet;
+      img.src = `https://www.nytimes.com/${image}`
+      li.prepend(img);
+      responseContainer.appendChild(li);
+    }
+  }
 }
